cbce81662001f711222c38f4cde54ff7caafba31
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-05-18 21:58:07 -0300

    fix: alterações parciais em DentistaService

--- src/main/java/com/sorrisoperfeito/service/DentistaService.java
+++ src/main/java/com/sorrisoperfeito/service/DentistaService.java
@@ -1,10 +1,12 @@
 package com.sorrisoperfeito.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sorrisoperfeito.exceptions.CadastroException;
 import com.sorrisoperfeito.model.Dentista;
 import com.sorrisoperfeito.repository.DentistaRepository;
 
@@ -12,13 +14,25 @@ import com.sorrisoperfeito.repository.DentistaRepository;
 public class DentistaService {
 
 	@Autowired
-	private DentistaRepository repository; 
+	private DentistaRepository dentistaRepository; 
 	
 	public List<Dentista> findAll(){
-		return repository.findAll();
+		return dentistaRepository.findAll();
 	}
 	
-	public Dentista findById(Integer id) {
-		return repository.findById(id).get();
+	public Optional<Dentista> findById(Integer id) {
+		return dentistaRepository.findById(id);
 	}
+	
+	public Boolean existsById(Integer id) {
+		return dentistaRepository.existsById(id);
+	}
+	
+	public Dentista createDentista(Dentista dentista) throws CadastroException{
+		if(dentista.getIdDentista() != null) {
+			throw new CadastroException("O Id do dentista precisa ser nulo!");
+		}	
+		return dentistaRepository.save(dentista);
+	}
+	
 }
