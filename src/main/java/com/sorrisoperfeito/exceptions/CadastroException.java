4c1df353e441b1e1df340fd259c2e77c64dc0794
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-05-18 21:59:30 -0300

    feat: adicionada CadastroException

---
+++ src/main/java/com/sorrisoperfeito/exceptions/CadastroException.java
@@ -0,0 +1,11 @@
+package com.sorrisoperfeito.exceptions;
+
+public class CadastroException extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+
+	public CadastroException(String msg) {
+		super(msg);
+	}
+
+}
